0f899fd5eddf9cdb77446a8ff46ad0d7176b3e85
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2025-03-01 20:49:52 -0500

    updated chicory, updated wasm bridge chicory
    new version of chicory sdk
    
    "simplified" using block for wasm bridge, chicory. re-added the bridge back to the scene

--- Wasm Experiment/Assets/Scripts/WasmBridgeChicory.cs
+++ Wasm Experiment/Assets/Scripts/WasmBridgeChicory.cs
@@ -19,23 +19,23 @@ public class WasmBridgeChicory : MonoBehaviour
         var result = new String(plugin.call("greet", input.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
          */
         // becomes
-        using (AndroidJavaObject urlString = new AndroidJavaObject("java.lang.String", "https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm"))
-        using (AndroidJavaClass ManifestWasmClass = new AndroidJavaClass("org.extism.chicory.sdk.ManifestWasm"))
-        using (AndroidJavaObject ManifestWasmBuilder = ManifestWasmClass.CallStatic<AndroidJavaObject>("fromUrl", urlString))
-        using (AndroidJavaObject ManifestWasmObj = ManifestWasmBuilder.Call<AndroidJavaObject>("build"))
-        using (AndroidJavaClass ManifestClass = new AndroidJavaClass("org.extism.chicory.sdk.Manifest"))
-        using (AndroidJavaObject ManifestBuilder = ManifestClass.CallStatic<AndroidJavaObject>("ofWasms", ManifestWasmObj))
-        using (AndroidJavaObject ManifestObj = ManifestBuilder.Call<AndroidJavaObject>("build"))
-        using (AndroidJavaClass PluginClass = new AndroidJavaClass("org.extism.chircory.sdk.Plugin"))
-        using (AndroidJavaObject PluginBuilder = PluginClass.CallStatic<AndroidJavaObject>("ofManifest", ManifestObj))
-        using (AndroidJavaObject PluginObj = PluginBuilder.Call<AndroidJavaObject>("build"))
-        using (AndroidJavaClass StandardCharsets = new AndroidJavaClass("java.nio.charset.StandardCharsets"))
-        using (AndroidJavaObject UTF_8 = StandardCharsets.GetStatic<AndroidJavaObject>("UTF_8"))
         {
-            AndroidJavaObject input = new AndroidJavaObject("java.lang.String", "Hello World");
-            AndroidJavaObject pluginFunc = new AndroidJavaObject("java.lang.String", "greet");
-            AndroidJavaObject resultBytes = PluginObj.Call<AndroidJavaObject>("call", pluginFunc, input.Call<AndroidJavaObject>("getBytes", UTF_8));
-            AndroidJavaObject resultString = new AndroidJavaObject("java.lang.String", resultBytes, UTF_8);
+            using AndroidJavaObject urlString = new AndroidJavaObject("java.lang.String", "https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm");
+            using AndroidJavaClass ManifestWasmClass = new AndroidJavaClass("org.extism.chicory.sdk.ManifestWasm");
+            using AndroidJavaObject ManifestWasmBuilder = ManifestWasmClass.CallStatic<AndroidJavaObject>("fromUrl", urlString);
+            using AndroidJavaObject ManifestWasmObj = ManifestWasmBuilder.Call<AndroidJavaObject>("build");
+            using AndroidJavaClass ManifestClass = new AndroidJavaClass("org.extism.chicory.sdk.Manifest");
+            using AndroidJavaObject ManifestBuilder = ManifestClass.CallStatic<AndroidJavaObject>("ofWasms", ManifestWasmObj);
+            using AndroidJavaObject ManifestObj = ManifestBuilder.Call<AndroidJavaObject>("build");
+            using AndroidJavaClass PluginClass = new AndroidJavaClass("org.extism.chircory.sdk.Plugin");
+            using AndroidJavaObject PluginBuilder = PluginClass.CallStatic<AndroidJavaObject>("ofManifest", ManifestObj);
+            using AndroidJavaObject PluginObj = PluginBuilder.Call<AndroidJavaObject>("build");
+            using AndroidJavaClass StandardCharsets = new AndroidJavaClass("java.nio.charset.StandardCharsets");
+            using AndroidJavaObject UTF_8 = StandardCharsets.GetStatic<AndroidJavaObject>("UTF_8");
+            using AndroidJavaObject input = new AndroidJavaObject("java.lang.String", "Hello World");
+            using AndroidJavaObject pluginFunc = new AndroidJavaObject("java.lang.String", "greet");
+            using AndroidJavaObject resultBytes = PluginObj.Call<AndroidJavaObject>("call", pluginFunc, input.Call<AndroidJavaObject>("getBytes", UTF_8));
+            using AndroidJavaObject resultString = new AndroidJavaObject("java.lang.String", resultBytes, UTF_8);
             string localResults = resultString.ToString();
             Debug.Log(localResults);
         }
